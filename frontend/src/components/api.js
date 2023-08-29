378dbd1a0eecbecaa255d1c608ef72976bf91c7a
JeffAlden <134986403+JeffAlden@users.noreply.github.com>
2023-08-30 00:43:15 +0800

    Update api.js

--- frontend/src/components/api.js
+++ frontend/src/components/api.js
@@ -1,6 +1,5 @@
 import axios from 'axios';
 
-
 const baseURL = process.env.REACT_APP_API_URL || 'https://express-vercel-app-sigma.vercel.app' || 'http://localhost:3006';
 
 const api = axios.create({
