f5192d4058739efd118b6ec13053bb1fa560d3cc
JeffAlden <134986403+JeffAlden@users.noreply.github.com>
2023-08-31 11:54:10 +0800

    Update Navbar.js
    added id for style

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -38,97 +38,69 @@ const Navbar = ({ onCartClick }) => {
 
     const location = useLocation();
     const isMainPage = location.pathname === '/';
-
+    
     return (
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
-            <div className="container py-2">
-                <Link className="navbar-brand" to="/" onClick={scrollToTop}>CodeBrew Coffee Shop</Link>
-                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon"></span>
-                </button>
-                <div className="collapse navbar-collapse" id="navbarNav">
-                    <ul className="navbar-nav ms-auto align-items-center">
-                        {/* Conditional rendering for 'About' link */}
+        <div className="container py-2">
+            <Link className="navbar-brand" to="/" onClick={scrollToTop}>CodeBrew Coffee Shop</Link>
+            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse" id="navbarNav">
+                <ul className="navbar-nav ms-auto align-items-center">
+                    <li className="nav-item">
+                        <ScrollLink className="nav-link text-nowrap" to="about" smooth={true} duration={500} name="about">
+                            About
+                        </ScrollLink>
+                    </li>
+                    <li className="nav-item">
+                        <ScrollLink className="nav-link text-nowrap" to="services" smooth={true} duration={500} name="services">
+                            Services
+                        </ScrollLink>
+                    </li>
+                    <li className="nav-item">
+                        <ScrollLink className="nav-link text-nowrap" to="products" smooth={true} duration={500} name="products">
+                            Products
+                        </ScrollLink>
+                    </li>
+                    <li className="nav-item">
+                        <ScrollLink className="nav-link text-nowrap" to="contact" smooth={true} duration={500} name="contact">
+                            Contact Us
+                        </ScrollLink>
+                    </li>
+                    {/* Auth-related buttons */}
+                    {isAuthenticated ? (
                         <li className="nav-item">
-                            {isMainPage ? (
-                                <ScrollLink className="nav-link text-nowrap" to="about" smooth={true} duration={500}>
-                                    About
-                                </ScrollLink>
-                            ) : (
-                                <Link className="nav-link text-nowrap" to="/#about">
-                                    About
-                                </Link>
-                            )}
+                            <button type="button" className="btn btn-outline-primary mx-2" onClick={logout}>Logout</button>
                         </li>
-                        {/* Conditional rendering for 'Services' link */}
-                        <li className="nav-item">
-                            {isMainPage ? (
-                                <ScrollLink className="nav-link text-nowrap" to="services" smooth={true} duration={500}>
-                                    Services
-                                </ScrollLink>
-                            ) : (
-                                <Link className="nav-link text-nowrap" to="/#services">
-                                    Services
-                                </Link>
-                            )}
-                        </li>
-                        {/* Conditional rendering for 'Products' link */}
-                        <li className="nav-item">
-                            {isMainPage ? (
-                                <ScrollLink className="nav-link text-nowrap" to="products" smooth={true} duration={500}>
-                                    Products
-                                </ScrollLink>
-                            ) : (
-                                <Link className="nav-link text-nowrap" to="/#products">
-                                    Products
-                                </Link>
-                            )}
-                        </li>
-                        {/* Conditional rendering for 'Contact Us' link */}
-                        <li className="nav-item">
-                            {isMainPage ? (
-                                <ScrollLink className="nav-link text-nowrap" to="contact" smooth={true} duration={500}>
-                                    Contact Us
-                                </ScrollLink>
-                            ) : (
-                                <Link className="nav-link text-nowrap" to="/#contact">
-                                    Contact Us
-                                </Link>
-                            )}
-                        </li>
-                        {/* Auth-related buttons */}
-                        {isAuthenticated ? (
+                    ) : (
+                        <>
                             <li className="nav-item">
-                                <button type="button" className="btn btn-outline-primary mx-2" onClick={logout}>Logout</button>
+                                <Link to="/login">
+                                    <button type="button" className="btn btn-outline-primary mx-2">Login</button>
+                                </Link>
                             </li>
-                        ) : (
-                            <>
-                                <li className="nav-item">
-                                    <Link to="/login">
-                                        <button type="button" className="btn btn-outline-primary mx-2">Login</button>
-                                    </Link>
-                                </li>
-                                <li className="nav-item">
-                                    <Link to="/signup">
-                                        <button type="button" className="btn btn-primary mx-2">Sign Up</button>
-                                    </Link>
-                                </li>
-                            </>
-                        )}
-                        {/* Shopping Cart */}
-                        <li className="nav-item d-flex align-items-center">
-                            <span onClick={onCartClick} className="nav-link" style={{cursor: "pointer"}}>
-                                <FontAwesomeIcon icon={faShoppingCart} className="mx-2" />
-                                {totalQuantity > 0 && (
-                                    <span className="badge bg-danger">{totalQuantity}</span>
-                                )}
-                            </span>
-                        </li>
-                    </ul>
-                </div>
+                            <li className="nav-item">
+                                <Link to="/signup">
+                                    <button type="button" className="btn btn-primary mx-2">Sign Up</button>
+                                </Link>
+                            </li>
+                        </>
+                    )}
+                    {/* Shopping Cart */}
+                    <li className="nav-item d-flex align-items-center">
+                        <span onClick={onCartClick} className="nav-link" style={{cursor: "pointer"}}>
+                            <FontAwesomeIcon icon={faShoppingCart} className="mx-2" />
+                            {totalQuantity > 0 && (
+                                <span className="badge bg-danger">{totalQuantity}</span>
+                            )}
+                        </span>
+                    </li>
+                </ul>
             </div>
-        </nav>
-    );
+        </div>
+    </nav>
+);
 };
 
 export default Navbar;
